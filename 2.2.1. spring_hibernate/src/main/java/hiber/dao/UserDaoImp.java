611f408656c4087d0941241ad11acc50c0232efb
Mikhail Altunin <altunin@list.ru>
2023-03-24 17:07:48 +0300

    ver. 6

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -4,8 +4,6 @@ import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -33,9 +31,9 @@ public class UserDaoImp implements UserDao {
     @Override
     public User getUserByCarModelAndSeries(String model, int series) {
         String hql = "FROM User u WHERE u.car.model = :model AND u.car.series = :series";
-        Query query = sessionFactory.openSession().createQuery(hql, User.class);
+        TypedQuery<User> query = sessionFactory.openSession().createQuery(hql, User.class);
         query.setParameter("model", model);
         query.setParameter("series", series);
-        return (User) query.getSingleResult();
+        return query.getSingleResult();
     }
 }
